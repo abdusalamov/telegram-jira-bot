3e1a8dcc7db2ae1ba4c0926417c9ff57a91386b6
Renat Abdusalamov <renat@abdusalamov.com>
2019-02-22 13:44:21 +0300

    add Dockerfile

--- src/helpers.ts
+++ src/helpers.ts
@@ -12,15 +12,11 @@ interface Issue {
   }
 }
 
-export const formatMessage = (obj: Issue, withDescription?: boolean) => {
-  console.log(obj.fields);
+export const formatMessage = (obj: Issue) => {
   const msg = [];
   msg.push(`<a href="https://${process.env.JIRA_HOSTNAME}/browse/${obj.key}">${obj.key} ${obj.fields.summary}</a>`);
   msg.push(`Assignee: <b>${obj.fields.assignee.displayName}</b>`);
   msg.push(`Status: <b>${obj.fields.status.name}</b>`);
-  if (withDescription) {
-    msg.push(`Assigne: <b>${obj.fields.description}</b>`);
-  }
   return msg.join('\n');
 };
 
